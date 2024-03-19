02bcf2a47199cd995f81e19dd272df792d92628b
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2024-03-20 01:46:29 +0300

    res-original

--- webpack/production.ts
+++ webpack/production.ts
@@ -29,7 +29,7 @@ export default {
         new CopyPlugin({
             patterns: [
                 {from: './*.(html|css)'},
-                {from: './res/**/*.(png|jpg|mp3|json)', filter: (path) => !path.includes('/res/original/')},
+                {from: './res/**/*.(png|jpg|mp3|json)'},
             ]
         })
     ]
