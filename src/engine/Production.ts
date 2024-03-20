34f25799940b158e41d54a47f2a9dbf6d99b8907
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2024-03-20 16:43:05 +0300

    docs

--- src/engine/Production.ts
+++ src/engine/Production.ts
@@ -3,7 +3,7 @@ import {Environment} from "./Environment"
 
 export function quickfixSpritesheetForProduction(asset: Spritesheet) {
     for (const frame of Object.keys(asset.data.frames)) {
-        const key = `${window.origin}/${Environment.basePath}/${frame}`
+        const key = `${window.location.href}${Environment.basePath}/${frame}`
         const value = Assets.cache.get(frame)
         Assets.cache.set(key, value)
     }
