a0b91919c841f924dbb483ea29094ebb2ddef4db
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2024-03-27 17:22:35 +0300

    score example

--- examples/score.ts
+++ examples/score.ts
@@ -1,89 +1,87 @@
-import {bootstrap, context, startTimer, View, viewOf} from "../src/engine"
-import {Container, Graphics, Sprite, Texture} from "pixi.js"
-import gsap, {Power2, Circ} from "gsap"
-import {
-    blockPng,
-    boosterDynamitePng,
-    particlesBlockPng,
-    particlesGlitterPng,
-    particlesShinePng
-} from "../res"
+import {bootstrap, context, delay, startTimer} from "../src/engine"
 import Image from "../src/ui/views/Image"
-import {Emitter, upgradeConfig} from "@pixi/particle-emitter"
 import {emitParticles} from "../src/engine/Particles"
 import Layer from "../src/ui/views/Layer"
-import {greenColor, purpleColor, redColor, yellowColor} from "../src/features/style"
+import {redColor} from "../src/features/style"
+import {
+    blockPng,
+    particlesBlockPng
+} from "../res"
+import {Container} from "pixi.js"
 
 bootstrap(score)
 
 async function score() {
     const COLOR: string = redColor
     await context.loader.load([particlesBlockPng])
-    const particles: View = context.layout.append(Layer({
+    const block: Image = Layer({size: {width: 450, height: 800}}).addChild(new Image({
         position: {x: 225, y: 400},
-        size: {width: 450, height: 800}
-    })).append(new Image({
-        position: {x: 225, y: 30},
         anchor: {x: 0.5, y: 0.5},
-        size: {width: 171 / 2, height: 192 / 2},
+        size: {width: 171 / 3, height: 192 / 3},
         foreground: blockPng,
         tint: COLOR
     }))
-    emitParticles(particles.container, {
-        textures: particlesBlockPng,
-        rows: 2,
-        columns: 1,
-        alpha: {
-            start: 1,
-            end: 0.06
-        },
-        scale: {
-            start: 1,
-            end: 1,
-            minimumScaleMultiplier: 1
-        },
-        color: {
-            start: COLOR,
-            end: COLOR
-        },
-        speed: {
-            start: 1,
-            end: 1,
-            minimumSpeedMultiplier: 1
-        },
-        acceleration: {
-            x: 0,
-            y: 400
-        },
-        maxSpeed: 0,
-        startRotation: {
-            min: 90,
-            max: 90
-        },
-        noRotation: true,
-        rotationSpeed: {
-            min: 0,
-            max: 0
-        },
-        lifetime: {
-            min: 2,
-            max: 3
-        },
-        blendMode: "normal",
-        frequency: 1,
-        emitterLifetime: -1,
-        maxParticles: 25,
-        pos: {
-            x: 0,
-            y: 0
-        },
-        addAtBack: false,
-        spawnType: "rect",
-        spawnRect: {
-            x: -200,
-            y: -5,
-            w: 400,
-            h: 10
-        }
+    const particles: Container = Layer({position: {x: 225, y: 400}, size: {width: 450, height: 800}})
+    block.eventMode = 'dynamic'
+    block.on('pointerdown', () => {
+        block.visible = false
+        delay(0.65, () => block.visible = true)
+        emitParticles(particles, {
+            textures: particlesBlockPng,
+            rows: 2,
+            columns: 2,
+            "alpha": {
+                "start": 1,
+                "end": 0.88
+            },
+            "scale": {
+                "start": 1.2,
+                "end": 0,
+                "minimumScaleMultiplier": 1
+            },
+            "color": {
+                "start": COLOR,
+                "end": COLOR
+            },
+            "speed": {
+                "start": 600,
+                "end": 600,
+                "minimumSpeedMultiplier": 1
+            },
+            "acceleration": {
+                "x": 0,
+                "y": 3600
+            },
+            "maxSpeed": 0,
+            "startRotation": {
+                "min": -90 - 45,
+                "max": -90 + 45
+            },
+            "noRotation": true,
+            "rotationSpeed": {
+                "min": 45,
+                "max": 360
+            },
+            "lifetime": {
+                "min": 0.88,
+                "max": 0.88
+            },
+            "blendMode": "normal",
+            "frequency": 0.001,
+            "emitterLifetime": 0.5,
+            "maxParticles": 16,
+            "pos": {
+                "x": 0,
+                "y": 0
+            },
+            "addAtBack": false,
+            "spawnType": "rect",
+            "spawnRect": {
+                "x": -16,
+                "y": -16 - 16 - 16 - 16,
+                "w": 32 - 8 + 12 + 12,
+                "h": 48 + 12 + 12
+            }
+        })
     })
 }
--- src/ui/views/Layer.ts
+++ src/ui/views/Layer.ts
@@ -1,14 +1,18 @@
-import {IPointData, ISize} from "pixi.js"
-import {View} from "../../engine"
+import {Container, IPointData, ISize} from "pixi.js"
+import {context, View} from "../../engine"
 
 type LayerOptions = {
-    position: IPointData
+    position?: IPointData
     size: ISize
 }
 
-export default function Layer(options: LayerOptions): View {
-    const {position, size} = options
+// IMPORTANT used for tests only
+export default function Layer(options: LayerOptions): Container {
+    const {position = {x: 0, y: 0}, size} = options
     const view: View = new View(size)
-    view.container.position = position
-    return view
+    const layer: Container = new Container()
+    layer.position = position
+    view.append(layer)
+    context.layout.append(view)
+    return layer
 }
